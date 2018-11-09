8904016f2115a9b267507a845605c81e07e70bfe
neilmyers <mmyersneil@gmail.com>
2018-11-09 16:45:26 -0700

    sign up form and gray back button

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
+import SignInForm from './signUpForm';
+import PageTitle from '../pageTitle';
+
 class SignUp extends Component {
+
+  onSubmit = (fields) => {
+    console.log(fields)
+  }
+
   render() {
     return (
       <div className="sign-up">
-        sign up
+        <PageTitle className="sign-up__page-title" title="Register"/>
+        <SignInForm onSubmit={this.onSubmit} className="sign-up__form"/>
       </div>
     );
   }
